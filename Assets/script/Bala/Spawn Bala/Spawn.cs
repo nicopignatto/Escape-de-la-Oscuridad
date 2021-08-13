e852c9f3a8230961071eae35d535e81352474466
FerChaz <ferman.pl@gmail.com>
2021-08-13 19:21:04 -0300

    Agregado disparo automatico con cooldown variable :D

--- Assets/script/Bala/Spawn Bala/Spawn.cs
+++ Assets/script/Bala/Spawn Bala/Spawn.cs
@@ -6,14 +6,23 @@ public class Spawn : MonoBehaviour
 {
     [Header("linkeos/referencias a objetos")]
     [SerializeField] private GameObject balita;
+    [SerializeField] private float shootCooldown;
+    private float shootCooldownActual;
 
+    private void Start() {
+        shootCooldownActual = 0;
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0))
         {
-            Instantiate(balita,transform.position,Quaternion.identity);
-           
+            if (shootCooldownActual <= 0) 
+            {
+                shootCooldownActual = shootCooldown;
+                Instantiate(balita,transform.position,Quaternion.identity);
+            }
         }
+        shootCooldownActual -= Time.deltaTime;
     }
 
 }
