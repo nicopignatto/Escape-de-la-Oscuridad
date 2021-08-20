5669fab51755c96ae4461cb5aa23e4e7e1834b6a
agus5312 <agusalonso2000@hotmail.es>
2021-08-20 20:11:48 -0300

    Camara se detiene en los limites

--- Assets/script/Camara/CamaraMuevePosJugador.cs
+++ Assets/script/Camara/CamaraMuevePosJugador.cs
@@ -6,17 +6,40 @@ public class CamaraMuevePosJugador : MonoBehaviour
 {
     [Header("referencias a objetos/componentes de objetos")]
     [SerializeField] private Transform posDelJugador;
+    
+    public float limiteDerecho;
+    public float limiteIzquierdo;
+    public float limiteInferior;
+
     private void Start()
     {
-        EnfocarJugador();
+        
     }
     private void Update()
     {
-        EnfocarJugador();
+        if(posDelJugador.position.x <= limiteIzquierdo)
+        {
+            
+            transform.position = new Vector3(limiteIzquierdo, transform.position.y, transform.position.z);
+        }
+        
+     
+        if (posDelJugador.position.x >= limiteDerecho)
+        {
+            
+            transform.position = new Vector3(limiteDerecho, limiteInferior, transform.position.z);
+        }
+        
+
+
+        if (posDelJugador.position.x > limiteIzquierdo && posDelJugador.position.x < limiteDerecho)
+        {
+            EnfocarJugador();
+        }
     }
 
     private void EnfocarJugador()
     {
-        transform.position = new Vector3(posDelJugador.position.x, posDelJugador.position.y, transform.position.z);
+        transform.position = new Vector3(posDelJugador.position.x, limiteInferior, transform.position.z);
     }
 }
