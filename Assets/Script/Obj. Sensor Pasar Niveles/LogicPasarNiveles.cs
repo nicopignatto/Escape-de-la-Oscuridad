668c302218dcce6ede1cea0ca9485c0af7feaba9
nicopignatto <81388990+nicopignatto@users.noreply.github.com>
2021-10-01 06:03:52 -0300

    Agregue un sensor para pasar entre niveles
    Cree un objeto vacio(que lo hice prefab) q hace que solamente cuando el jugador triggee con él va a pasar a la siguiente escena(fase del nivel 1,en este caso).Me falta arreglar un par de cosas del jugador pero es solo pasar referencias y agregar la oscuridad.

---
+++ Assets/Script/Obj. Sensor Pasar Niveles/LogicPasarNiveles.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LogicPasarNiveles : MonoBehaviour
+{
+    [Header("Booleano que indica si se paso el nivel")]
+    [SerializeField] private bool pasasteElNivel;//este booleano sirve para indicar si se completo el nivel y pasar a la escena siguiente
+
+    [Header("Indice de Escena Actual")]//esto indica el indice de la escena actual,al que despues más adelante se le suma "1" para poder avanzar a la siguiente escena 
+    [SerializeField] private int indiceEscenaActual;
+
+    [Header("Nombre de Escena Actual")]
+    [SerializeField] private string nombreEscena;
+
+    [Header("Linkeos")]
+    [SerializeField] private GameObject jugador; //esta variable hace referencia al jugador
+
+    /*private Scene escenaDeseada;*/  //esta variable hacia referencia a la escena y eso no permite acceder a diferentes aspectos de la escena(POR FAVOR NO BORRAR ESTA LINEA)
+    private void Start()
+    {
+        Debug.Log("el indice de la escena actual es:" + indiceEscenaActual);
+        pasasteElNivel = false;
+    }
+    private void Update()
+    {
+        PasarAlSiguienteNivel();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(indiceEscenaActual + 1);
+        }
+    }
+
+    private void PasarAlSiguienteNivel()//esto sirve mas como opcion de dev.
+    {
+        if (pasasteElNivel == true)
+        {
+            SceneManager.LoadScene(indiceEscenaActual + 1);
+        }
+    }
+
+}
+/*escenaDeseada.buildIndex*/
